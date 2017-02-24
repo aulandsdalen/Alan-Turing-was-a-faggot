fd2b3e72c124c1b44e605f509d937e7864b528b1
aulandsdalen <aulandsdalen@gmail.com>
2017-02-24 22:34:06 +0300

    initial commit

---
+++ tm.rb
@@ -0,0 +1,22 @@
+class TMInterpreter
+	@@commandsTape = []
+	@@memoryTape = []
+
+
+	def parseSourceFile(file)
+	end
+
+	
+
+	def printCommands
+		puts commandsTape
+	end
+
+	def printMem
+		puts memoryTape
+	end
+
+	def printAll
+		# join them perhaps? 
+	end
+end
