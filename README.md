2789680ed720605f8328928432a51db5facc1b3d
aulandsdalen <aulandsdalen@gmail.com>
2017-02-25 14:19:26 +0300

    cond jump name fixed, first working implementations; no loops yet

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ inc | increment the byte at the DP
 dec | decrement the byte at the DP 
 printb | print byte at the DP 
 getb | get one byte from input, store at the DP  
-jz | jump after the matching `end` command if data at the DP is non-zero 
+jnz | jump after the matching `end` command if data at the DP is non-zero 
 endz | if data at the DP is non-zero, then jump back to matching `jz`  
--- tm.rb
+++ tm.rb
@@ -1,50 +1,95 @@
 class TMInterpreter
 	@@commandsTape = []
 	@@memoryTape = []
+	@@operationsExecuted
 
+	def initialize(file)
+		@@operationsExecuted = 0
+		@@dataPointer = 0
+		@@instructionPointer = 0
+		_length = parseSourceFile(file)
+		# initialize memory tape with cmdtape size
+		_length.times do
+			@@memoryTape.push(0)
+		end
+	end
 
 	def printCommands
-		puts commandsTape
+		puts @@commandsTape
 	end
 
 	def printMem
-		puts memoryTape
+		puts @@memoryTape
 	end
 
-	def printAll
-		# join them perhaps? 
+	def printExecStats
+		puts "Executed #{@@operationsExecuted} ops "
+		puts "Mem: #{@@memoryTape}"
+		puts "Commands: #{@@commandsTape}"
+		puts "DP: #{@@dataPointer}, IP: #{@@instructionPointer}"
 	end
 
 	def run
+		executeCommand
+		puts "-- finished --"
+		printExecStats
 	end
 
 	private 
 
+	@@dataPointer
+	@@instructionPointer
 	def parseSourceFile(file)
+		_tmp = IO.readlines(file)
+		_tmp.each do |x| 
+			x.chomp! # get rid of newlines
+		end
+		@@commandsTape = _tmp
+		@@commandsTape.length
 	end
 
-	def executeCommand(position)
-		command = @@commandsTape[position]
-		case command
-		when 'movr'
-			executeCommand((position+1))
-		when 'movl'
-			executeCommand((position-1))
-		when 'inc'
-			@@memoryTape[position] += 1
-		when 'dec'
-			@@memoryTape[position] -= 1
-		when 'printb'
-			puts @@memoryTape[position]
-		when 'getb'
-			tmp = gets
-			@@memoryTape[position] = tmp[0].to_i
-		when 'jz'
-			# do something ok?
-		when 'endz'
-			# loops are to be implemented later 
+	def executeCommand
+		unless @@instructionPointer >= @@commandsTape.length
+			_command = @@commandsTape[@@instructionPointer]
+			puts "executing #{_command}, IP: #{@@instructionPointer}, DP: #{@@dataPointer}"
+			@@operationsExecuted += 1
+			case _command
+			when 'movr'
+				@@dataPointer += 1
+				@@instructionPointer += 1
+				executeCommand
+			when 'movl'
+				@@dataPointer -= 1
+				@@instructionPointer += 1
+				executeCommand
+			when 'inc'
+				@@memoryTape[@@dataPointer] += 1
+				@@instructionPointer += 1
+				executeCommand
+			when 'dec'
+				@@memoryTape[@@dataPointer] -= 1
+				@@instructionPointer += 1
+				executeCommand
+			when 'printb'
+				puts "#{@@memoryTape[@@dataPointer]} at DP: #{@@dataPointer}"
+				@@instructionPointer += 1
+				executeCommand
+			when 'getb'
+				tmp = gets
+				@@memoryTape[position] = tmp[0].to_i
+			when 'jnz' # jump over the endz statement if DP points at non-zero value
+				if @@memoryTape[position] != 0
+					# seek until encounter 'endz' and call executeCommand w/that position
+				else
+					executeCommand((position+1))
+				end
+			when 'endz'
+				# loops are to be implemented later 
+			else 
+				raise NoMethodError, "don't know how to interpret #{_command}"
+			end
 		else 
-			raise NoMethodError, "don't know how to interpret #{command}"
+			return 0
 		end
 	end
 end
